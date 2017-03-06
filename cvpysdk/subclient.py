d5dbebb3279978e1868737362483e2ce6811d8bf
sathaley1 <s.athaley@gmail.com>
2017-03-06 17:38:49 +0530

    Support for cloud apps subclient (#4)
    * Added support for Cloud Apps subclient
    
    * Updated exception message for commcell

--- cvpysdk/subclient.py
+++ cvpysdk/subclient.py
@@ -9,8 +9,8 @@
 
 """Main file for performing sublcient operations.
 
-Subclients, Subclient, FileSystemSubclient,
-and VirtualServerSubclient are 4 classes defined in this file.
+Subclients, Subclient, FileSystemSubclient, VirtualServerSubclient,
+and CloudAppsSubclient are 5 classes defined in this file.
 
 Subclients: Class for representing all the subclients associated with a backupset
 
@@ -21,6 +21,9 @@ FileSystemSubclient: Derived class from Subclient Base class, representing a fil
 
 VirtualServerSubclient: Derived class from Subclient Base class, representing a
                             virtual server subclient, and to perform operations on that subclient
+
+CloudAppsSubclient: Derived class from Subclient Base class, representing a
+                        cloud apps subclient, and to perform operations on that subclient
 
 Subclients:
     __init__(class_object)      --  initialise object of subclients object associated with
@@ -103,6 +106,12 @@ VirtualServerSubclient:
 
     _set_subclient_content_()   --  sets the content of a virtual server subclient
 
+
+CloudAppsSubclient:
+    _get_subclient_content_()   --  gets the content of a cloud apps subclient
+
+    _set_subclient_content_()   --  sets the content of a cloud apps subclient
+
 """
 
 from __future__ import absolute_import
@@ -163,7 +172,8 @@ class Subclients(object):
         # the appropriate class object will be initialized based on the agent
         self._subclients_dict = {
             'file system': FileSystemSubclient,
-            'virtual server': VirtualServerSubclient
+            'virtual server': VirtualServerSubclient,
+            'cloud apps': CloudAppsSubclient
         }
 
     def __str__(self):
@@ -1718,6 +1728,64 @@ class VirtualServerSubclient(Subclient):
                 }
 
                 content.append(virtual_server_dict)
+        except KeyError as err:
+            raise SDKException('Subclient', '102', '{} not given in content'.format(err))
+
+        return content
+
+
+class CloudAppsSubclient(Subclient):
+    """Derived class from Subclient Base class, representing a CloudApps subclient,
+        and to perform operations on that subclient."""
+
+    def _get_subclient_content_(self, subclient_properties):
+        """Gets the appropriate content from the Subclient relevant to the user.
+
+            Args:
+                subclient_properties (dict)  --  dictionary contatining the properties of subclient
+
+            Returns:
+                list - list of content associated with the subclient
+        """
+        content = []
+
+        for account in subclient_properties['content']:
+            temp_account = account["cloudconnectorContent"]["includeAccounts"]
+
+            content_dict = {
+                'SMTPAddress': temp_account["contentName"],
+                'display_name': temp_account["contentValue"]
+            }
+
+            content.append(content_dict)
+
+        return content
+
+    def _set_subclient_content_(self, subclient_content):
+        """Creates the list of content JSON to pass to the API to add/update content of a
+            Cloud Apps Subclient.
+
+            Args:
+                subclient_content (list)  --  list of the content to add to the subclient
+
+            Returns:
+                list - list of the appropriate JSON for an agent to send to the POST Subclient API
+        """
+        content = []
+
+        try:
+            for account in subclient_content:
+                temp_content_dict = {
+                    "cloudconnectorContent": {
+                        "includeAccounts": {
+                            "contentValue": account['display_name'],
+                            "contentType": 134,
+                            "contentName": account['SMTPAddress']
+                        }
+                    }
+                }
+
+                content.append(temp_content_dict)
         except KeyError as err:
             raise SDKException('Subclient', '102', '{} not given in content'.format(err))
 
