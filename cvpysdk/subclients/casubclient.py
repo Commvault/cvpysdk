547956fb9b21687674351dbd5891551892ba2e6b
Siddharth Athaley <sathaley@CommVault.com>
2017-03-30 12:08:28 +0530

    Out of Place Restore for CloudApps Subclient

--- cvpysdk/subclients/casubclient.py
+++ cvpysdk/subclients/casubclient.py
@@ -19,6 +19,8 @@ CloudAppsSubclient:
 
     _set_subclient_content_()   --  sets the content of a cloud apps subclient
 
+    restore_out_of_place()      --  runs out-of-place restore for the subclient
+
 """
 
 from ..exception import SDKException
@@ -78,3 +80,90 @@ class CloudAppsSubclient(Subclient):
             raise SDKException('Subclient', '102', '{} not given in content'.format(err))
 
         return content
+
+    def restore_out_of_place(
+            self,
+            client,
+            destination_path,
+            paths,
+            overwrite=True,
+            restore_data_and_acl=True):
+        """Restores the files/folders specified in the input paths list to the input client,
+            at the specified destionation location.
+
+            Args:
+                client                (str/object) --  either the name of the client or
+                                                           the instance of the Client
+
+                destination_path      (str)        --  full path of the restore location on client
+
+                paths                 (list)       --  list of full paths of
+                                                           files/folders to restore
+
+                overwrite             (bool)       --  unconditional overwrite files during restore
+                    default: True
+
+                restore_data_and_acl  (bool)       --  restore data and ACL files
+                    default: True
+
+            Returns:
+                object - instance of the Job class for this restore job
+
+            Raises:
+                SDKException:
+                    if client is not a string or Client instance
+
+                    if destination_path is not a string
+
+                    if paths is not a list
+
+                    if failed to initialize job
+
+                    if response is empty
+
+                    if response is not success
+        """
+        from ..client import Client
+
+        if not ((isinstance(client, str) or isinstance(client, Client)) and
+                isinstance(destination_path, str) and
+                isinstance(paths, list) and
+                isinstance(overwrite, bool) and
+                isinstance(restore_data_and_acl, bool)):
+            raise SDKException('Subclient', '101')
+
+        if isinstance(client, Client):
+            client = client
+        elif isinstance(client, str):
+            client = Client(self._commcell_object, client)
+        else:
+            raise SDKException('Subclient', '105')
+
+        paths = self._filter_paths(paths)
+
+        destination_path = self._filter_paths([destination_path], True)
+
+        if paths == []:
+            raise SDKException('Subclient', '104')
+
+        request_json = self._restore_json(
+            paths=paths,
+            in_place=False,
+            client=client,
+            destination_path=destination_path,
+            overwrite=overwrite,
+            restore_data_and_acl=restore_data_and_acl
+        )
+
+        request_json["taskInfo"]["subTasks"][0]["options"][
+            "restoreOptions"]['cloudAppsRestoreOptions'] = {
+            "instanceType": self._instance_object._ca_instance_type,
+            "googleRestoreOptions": {
+                "strDestUserAccount": destination_path,
+                "folderGuid": "",
+                "restoreToDifferentAccount": True,
+                "restoreToGoogle": True
+            }
+        }
+
+        return self._process_restore_request(request_json)
