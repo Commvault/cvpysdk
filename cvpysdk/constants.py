a732f8d5187bc91107ed37b086191a0b69ad309b
Jatin Goel <jatingoel1037@hotmail.com>
2017-08-18 16:15:28 +0530

    Update constants.py

--- cvpysdk/constants.py
+++ cvpysdk/constants.py
@@ -2,24 +2,27 @@
 # -*- coding: utf-8 -*-
 
 # --------------------------------------------------------------------------
-# Copyright ?2016 Commvault Systems, Inc.
+# Copyright ©2016 Commvault Systems, Inc.
 # See LICENSE.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
 
-"""Constants for SDK Operations.
+"""
+Constants for specifying the Hyper Visor Type for Virtual Server agents.
 
-HyperVisorType:
-        Enum class for Instance Names
+All supported hyper visors must be added to the HyperVisor class of type Enum.
+
+HyperVisorType:     Enum class for Instance Names
 
 """
 
-
 from enum import Enum
 
-class HyperVisorType(Enum):
-    """ Maps the Instance Hypervisor  Name for general Names """
+
+class HyperVisor(Enum):
+    """Class for mapping the Instance Name of a Virtual Server Agent Client to a general name."""
+
     VIRTUAL_CENTER = "VMware"
     MS_VIRTUAL_SERVER = "hyper-v"
     AZURE = "Azure"
-    AZURE_V2  = "Azure Resource Manager"
+    AZURE_V2 = "Azure Resource Manager"
