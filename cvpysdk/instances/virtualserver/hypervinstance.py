7c9652ca2b916f78def22cf982932e8da300b751
Sachin Shriram Pakhare <spakhare@commvault.com>
2017-10-27 16:08:27 +0530

    Virtual Server instances support

--- cvpysdk/instances/virtualserver/hypervinstance.py
+++ cvpysdk/instances/virtualserver/hypervinstance.py
@@ -16,76 +16,91 @@ HyperVInstance: Derived class from VirtualServer  Base class, representing a
 
 HyperVInstance:
 
-	__init__(agent_object,instance_name,instance_id)    --  initialize object of hyper-v Instance object associated with
-																the VirtualServer Instance
+    __init__(agent_object,instance_name,instance_id)    --  initialize object of hyper-v Instance
+                                                                object associated with the
+                                                                        VirtualServer Instance
 
-    _get_instance_properties()  						--  VirtualServer Instance class method overwritten to get Hyper-V Specific
-																instance properties as well
 
-	_set_instance_properties()							--	Hyper-V Instance class method  to set Hyper-V Specific
-																instance properties 
+    _get_instance_properties()                          --  VirtualServer Instance class method
+                                                            overwritten to get Hyper-V Specific
+                                                                         instance properties as well
+
+    _set_instance_properties()                          --  Hyper-V Instance class method  to
+                                                        set Hyper-V Specific instance properties
+
 
 """
 
 
 from ..vsinstance import VirtualServerInstance
-from ...exception import SDKException
 
 class HyperVInstance(VirtualServerInstance):
-	"""Class for representing an Hyper-V of the Virtual Server agent."""
-	
-	def __init__(self,agent_object, instance_name, instance_id=None):
-		"""Initialize the Instance object for the given Virtual Server instance.
+    """Class for representing an Hyper-V of the Virtual Server agent."""
+
+    def __init__(self, agent, instance_name, instance_id=None):
+        """Initialize the Instance object for the given Virtual Server instance.
 
             Args:
-                class_object (agent_object,instance_name,instance_id)  --  instance of the Agent class, instance name, instance id
+                class_object (agent_object,instance_name,instance_id)  --  instance of the
+                                                                                Agent class,
+                                                                                instance name,
+                                                                                instance id
 
-		"""
-		super(HyperVInstance,self).__init__(agent_object, instance_name, instance_id)
-		self._vendor_id(2)
+        """
+        super(HyperVInstance, self).__init__(agent, instance_name, instance_id)
+        self._vendor_id = 2
 
-	
-	def  _get_instance_properties(self):
-			"""Gets the properties of this instance.
 
-					Raises:
-							SDKException:
-									if response is empty
 
-									if response is not success
-			"""
-			super(VirtualServerInstance, self)._get_instance_properties()
-			self._server_name = []
-			self._get_instance_common_properties()
-			if 'virtualServerInstance' in self._properties:
-				_member_servers = self._properties["virtualServerInstance"]["associatedClients"]["memberServers"]
-				for _each_client in _member_servers:
-					client = _each_client['client']
-					if 'clientName' in client.keys():
-							self._server_name.append(str(client['clientName']))
-	
-	def set_instance_properties(self):
-			"""sets the Instance Property for this instance
-			
-			Raises:
-					SDKException:
-							if response is not success
-			
-			"""
-				
-			self._default_FBRUnix_MediaAgent(self._properties_dict)
-			self._vcPassword(self._properties_dict)
-			self._docker(self._properties_dict)
-			self._open_stack(self._properties_dict)
-			self._azure(self._properties_dict)
-			self._oracle_cloud(self._properties_dict)
-			self._azure_Resource_Manager(self._properties_dict)
-	
-			
-			request_json = self._prepare_instance_json()
-			
-			return self._update_instance_properties_request(request_json)
-			
-		
-			
-		 
+    def  _get_instance_properties(self):
+        """
+        Get the properties of this instance
+
+        Raise:
+            SDK Exception:
+                if response is not empty
+                if response is not success
+        """
+
+        super(HyperVInstance, self)._get_instance_properties()
+        self._server_name = []
+
+        if 'virtualServerInstance' in self._properties:
+            _member_servers = self._properties["virtualServerInstance"] \
+                                                ["associatedClients"]["memberServers"]
+            for _each_client in _member_servers:
+                client = _each_client['client']
+                if 'clientName' in client.keys():
+                    self._server_name.append(str(client['clientName']))
+
+
+    def _get_instance_properties_json(self):
+        """get the all instance related properties of this subclient.
+
+          Returns:
+               dict - all instance properties put inside a dict
+
+        """
+        instance_json = {
+            "instanceProperties":{
+                "isDeleted": False,
+                "instance": self._instance,
+                "instanceActivityControl": self._instanceActivityControl,
+                "virtualServerInstance": {
+                    "vsInstanceType": self._virtualserverinstance['vsInstanceType'],
+                    "associatedClients": self._virtualserverinstance['associatedClients'],
+                    "vmwareVendor": {}                           
+                    }
+                       }
+               }
+        return instance_json
+
+    @property
+    def server_name(self):
+        """getter for the domain name in the Hyper-V json"""
+        return self._server_name
+
+    @property
+    def server_host_name(self):
+        """getter for the domain name in the vmware vendor json"""
+        return self._server_name
